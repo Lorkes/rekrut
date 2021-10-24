20e2d339746eb9d27d14631c70028808791aee25
Eugen Martsinkovskyi <eugen.martsinkovskyi@stxnext.pl>
2021-10-24 15:50:00 +0200

    Admin panel created

--- djproj/cats/admin.py
+++ djproj/cats/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Cat, Hunting
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Cat)
+admin.site.register(Hunting)
