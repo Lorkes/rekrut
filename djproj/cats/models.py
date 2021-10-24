527b7efc99fc01aae872177c8eb754232c5c9d11
Eugen Martsinkovskyi <eugen.martsinkovskyi@stxnext.pl>
2021-10-24 15:47:06 +0200

    Models created, migrations have run

---
+++ djproj/cats/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class User(models.Model):
+    user_total_cats = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(4)])
+
+
+class Cat(models.Model):
+    cat_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    cat_name = models.CharField(max_length=100)
+    cat_coloration = models.CharField(max_length=200)
+    cat_sex = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')], default='M')
+
+
+class Hunting(models.Model):
+    cat_went = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    hunting_duration = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(72)]) # 1 hour - 3 days
+    hunting_prey = models.CharField(max_length=100)
+
